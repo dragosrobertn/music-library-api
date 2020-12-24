83a39c8f8132520caf9bc5253bc38d9aa7e0731e
Dario Milani <runner855@icloud.com>
2020-12-24 17:35:40 +0000

    artist created

--- src/app.js
+++ src/app.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const app = express();
-const Artist = require('./models/artist')
+const artistControllers = require('./controllers/artists');
+
 app.use(express.json());
 
 app.get('/', (req, res) => {
     res.json({ result: 'Hello World' });
 })
 
-app.post('/artists', (req, res) => {
-res.status(201).json({ result: req.body.id})
-})
+    app.post('/artists',  artistControllers.create);
+    
+
 
 module.exports = app;
--- src/controllers/artists.js
+++ src/controllers/artists.js
@@ -1,6 +1,12 @@
 const { Artist } = require('../models');
 
-exports.create = (req, res) => {
-    Artist.create(req.body).then(artist => res.status(201).json(artist));
+    exports.create = (req, res) => {
+        res.sendStatus(201).json(Artist);
 
-};
+        Artist.create(req.body).then(Artist => res.sendStatus(201).json(Artist));
+
+    };
+
+
+
+
