0c1395904b2b6eddd47401af1ca39a65d84953f6
Dario Milani <runner855@icloud.com>
2021-01-03 18:57:14 +0000

    reading artist completed

--- src/controllers/artists.js
+++ src/controllers/artists.js
@@ -1,6 +1,7 @@
 
  const { Artist } = require('../models');
 
+
 exports.create = (req, res) => {
 
   Artist.create(req.body).then(artist => res.status(201).json(artist));
@@ -14,8 +15,10 @@ exports.list = ( req, res ) => {
   };
 
   exports.getArtistById = (req, res) => {
+
     const  { artistId }  = req.params;
-  Artist.findByPk(artistId).then(artist => {
+    Artist.findByPk(artistId).then(artist => {
+
     if (!artist) {
       res.status(400).json({ error: 'The artist could not be found.' })
     } else {
